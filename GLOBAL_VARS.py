1b7e613343d9ddf3599c8745bdd9a1e98577b218
Javier Cebrián <jac.cebrian@gmail.com>
2023-08-13 19:46:21 +0200

    Add global vars

---
+++ GLOBAL_VARS.py
@@ -0,0 +1,20 @@
+
+input_name = 'ESCLM1900000019184A.csv'
+input_trend_name = 'ESCLM1900000019184A_trend.csv'
+input_res_name = 'ESCLM1900000019184A_res.csv'
+input_stats_name = 'ESCLM1900000019184A_stats.xlsx'
+input_compare_name = 'ESCLM1900000019184A_compare.xlsx'
+input_predict_name = 'ESCLM1900000019184A_predict.xlsx'
+
+unit_dict = {
+    'T. Max.': '[ºC]',
+    'T. Min.': '[ºC]',
+    'H. Max.': '[%]',
+    'H. Min.': '[%]',
+    'P. Max.': '[hPa]',
+    'P. Min.': '[hPa]',
+    'Vel.': '[Km/h]',
+    'Precipitación': '[l/m2]'
+}
+
+logo_temperature = 'termometro.png'
